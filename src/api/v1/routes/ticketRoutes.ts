1218513a320b58f39075aa23b39eebecd936aee2
jashanpreet <jsdihu55@academic.rrc.ca>
2026-02-01 18:18:09 -0600

    complete ticket routes for crud

--- src/api/v1/routes/ticketRoutes.ts
+++ src/api/v1/routes/ticketRoutes.ts
@@ -1,8 +1,19 @@
 import { Router } from "express";
-import { createTicketController } from "../controllers/ticketController";
+import {
+  createTicketController,
+  getAllTicketsController,
+  getTicketByIdController,
+  updateTicketController,
+  deleteTicketController,
+} from "../controllers/ticketController";
 
 const router = Router();
 
 router.post("/tickets", createTicketController);
+router.get("/tickets", getAllTicketsController);
+router.get("/tickets/:id", getTicketByIdController);
+router.put("/tickets/:id", updateTicketController);
+router.delete("/tickets/:id", deleteTicketController);
 
 export default router;
+
